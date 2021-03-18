8d7607059435550eb6aa935dd2577d5db7551dc7
User <izatert@gmail.com>
2021-03-18 16:47:49 +0300

    Commit

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,31 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+    @Autowired
+    private Timer timer;
+    public Timer getTimer(){
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
-        System.out.println("________________________");
+        System.out.println(timer.getTime());
+        System.out.println();
+        System.out.println("-------------------");
     }
 }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,9 +1,14 @@
 package app.model;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Scope("singleton")
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
-
     public Long getTime() {
         return nanoTime;
     }
